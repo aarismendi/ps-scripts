ab91d6a7c29517783d8fb31053bc0138ea741616
Andy Arismendi <andy_arismendi@hotmail.com>
2013-03-05 04:08:31 -0500

    More updates for ISE 3

--- mypsise.psm1
+++ mypsise.psm1
@@ -31,32 +31,50 @@
 
     $psISE.Options.ScriptPaneBackgroundColor = '#141414'
     $color_map = @{
-        'Attribute' = '#f8f8f8'
-        'Command' = '#dad085'
-        'CommandArgument' = '#f8f8f8' #Function name too.
-        'CommandParameter' = '#dad085'
-        'Comment' = '#5f5a60'
-        'GroupEnd' = '#f8f8f8'
-        'GroupStart' = '#f8f8f8'
-        'Keyword' = '#cda869'
-        'LineContinuation' = '#f8f8f8'
-        'LoopLabel' = '#f8f8f8'
-        'Member' = '#f8f8f8'
-        'NewLine' = '#cda869'
-        'Number' = '#cf6a4c'
-        'Operator' = '#cda869'
-        'Position' = '#f8f8f8'
-        'StatementSeparator' = '#f8f8f8'
-        'String' = '#8f9d6a'
-        'Type' = '#f8f8f8'
-        'Unknown' = '#f8f8f8'
-        'Variable' = '#7587a6'
+        Attribute = '#f8f8f8'
+        Command = '#dad085'
+        CommandArgument = '#f8f8f8' #Function name too.
+        CommandParameter = '#dad085'
+        Comment = '#5f5a60'
+        GroupEnd = '#f8f8f8'
+        GroupStart = '#f8f8f8'
+        Keyword = '#cda869'
+        LineContinuation = '#f8f8f8'
+        LoopLabel = '#f8f8f8'
+        Member = '#f8f8f8'
+        NewLine = '#cda869'
+        Number = '#cf6a4c'
+        Operator = '#cda869'
+        Position = '#f8f8f8'
+        StatementSeparator = '#f8f8f8'
+        String = '#8f9d6a'
+        Type = '#f8f8f8'
+        Unknown = '#f8f8f8'
+        Variable = '#7587a6'
+    }
+    $color_map2 = @{
+        Attribute = '#cda869'
+        CharacterData = '#7587a6'
+        Comment	= '#5f5a60'
+        CommentDelimiter = '#5f5a60'
+        ElementName	= '#dad085'
+        MarkupExtension	= '#8f9d6a'
+        Quote = '#8f9d6a'
+        QuotedString = '#8f9d6a'
+        Tag	= '#f8f8f8'
+        Text = '#8f9d6a'
     }
     foreach ($token_name in $color_map.Keys) {
         $color_value = $color_map.Item($token_name)
         $psise.Options.TokenColors[$token_name] = $color_value
         if (-not $is_IseV2) {
             $psise.Options.ConsoleTokenColors[$token_name] = $color_value
+        }
+    }
+    if (-not $is_IseV2) {
+        foreach ($token_name in $color_map2.Keys) {
+            $color_value = $color_map2.Item($token_name)
+            $psise.Options.XmlTokenColors[$token_name] = $color_value
         }
     }
 }
