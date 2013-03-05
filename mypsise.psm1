0d14ebf9484d440ea70351cd9f9ffd3a5d96dad3
Andy Arismendi <andy_arismendi@hotmail.com>
2013-03-05 03:32:57 -0500

    Updates for ISE 2.0

--- mypsise.psm1
+++ mypsise.psm1
@@ -1,20 +1,30 @@
 ﻿function Set-PSISEGitHubTheme {
+    $is_IseV2 = ($host.Name -eq 'Windows PowerShell ISE Host' -and $host.Version.ToString() -eq '2.0')
+
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
     $fonts = New-Object System.Drawing.Text.InstalledFontCollection
     $font_fams = $fonts.Families
     $font_prefs = New-Object System.Collections.Specialized.OrderedDictionary
-    $font_prefs.Add('Monaco', 10)
-    $font_prefs.Add('DejaVu Sans Mono', 10)
-    $font_prefs.Add('Lucida Console', 11)
-    $font_prefs.Add('Consolas', 11)
-    $font_prefs.Keys | % {
-        $pref_font = $_
+    if ($is_IseV2) {$font_bigger = 3} else {$font_bigger = 0}
+    $font_prefs.Add('Monaco', 10 + $font_bigger)
+    $font_prefs.Add('DejaVu Sans Mono', 10 + $font_bigger)
+    $font_prefs.Add('Lucida Console', 11 + $font_bigger)
+    $font_prefs.Add('Consolas', 11 + $font_bigger)
+    foreach ($pref_font in $font_prefs.Keys) {
         if ($font_fams -contains $pref_font) {
             $psISE.Options.FontName = $pref_font
             $psISE.Options.FontSize = $font_prefs.Item($pref_font)
             break
         }
     }
+
+    if ($is_IseV2) {
+        $psISE.Options.OutputPaneBackgroundColor = '#141414'
+        $psISE.Options.OutputPaneTextBackgroundColor = '#141414'
+        $psISE.Options.OutputPaneForegroundColor = '#f8f8f8'
+    }
+     
+    $psISE.Options.CommandPaneBackgroundColor = '#141414' 
     $psISE.Options.ScriptPaneBackgroundColor = '#141414'
     $psise.Options.TokenColors['Attribute'] = '#f8f8f8'
     $psise.Options.TokenColors['Command'] = '#dad085'
