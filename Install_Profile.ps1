d5cf16ee620ed50ef5cc1f73f80560380006ed2f
Andy Arismendi <andy_arismendi@hotmail.com>
2013-03-05 22:44:14 -0500

    When profile folder doesn't exist beforehand

--- Install_Profile.ps1
+++ Install_Profile.ps1
@@ -1,3 +1,4 @@
+$ErrorActionPreference = "Stop"
 $this_path = Split-Path ((Get-Variable MyInvocation -Scope 0).Value).MyCommand.Path
 $profile_dir = Split-Path $PROFILE
 $profile_files = @(
@@ -5,6 +6,9 @@ $profile_files = @(
 	'Microsoft.PowerShell_profile.ps1'
 	'profile.ps1'
 )
+if (-not [IO.Directory]::Exists($profile_dir)) {
+	New-Item -Path $profile_dir -ItemType Directory -Force | Out-Null
+}
 push-location $this_path
 foreach ($file in $profile_files) {
 	$src_file = (Get-Item $file).FullName
