f1f96c258c03c1e70e7bc4b73133b581bb707dfd
Andy Arismendi <andy_arismendi@hotmail.com>
2013-03-09 02:50:48 -0500

    Fix more indentation

--- Security.psm1
+++ Security.psm1
@@ -1,7 +1,7 @@
 function Test-IsAdmin {  
-    return (([Security.Principal.WindowsPrincipal] `
+	return (([Security.Principal.WindowsPrincipal] `
 				[Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(
-					[Security.Principal.WindowsBuiltInRole] "Administrator"))    
+					[Security.Principal.WindowsBuiltInRole] "Administrator"))	
 }
 
 function Unblock-File {
@@ -11,29 +11,29 @@ function Unblock-File {
 	)
 	begin {
 		Add-Type -Namespace Win32 -Name pinvoke -MemberDefinition @"
-            // http://msdn.microsoft.com/en-us/library/windows/desktop/aa363915(v=vs.85).aspx
-            [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+			// http://msdn.microsoft.com/en-us/library/windows/desktop/aa363915(v=vs.85).aspx
+			[DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
 			[return: MarshalAs(UnmanagedType.Bool)]
 			private static extern bool DeleteFile(string name);
-            public static int Win32DeleteFile(string filePath) {
-                bool is_deleted = DeleteFile(filePath);
-                return Marshal.GetLastWin32Error();
-            }
+			public static int Win32DeleteFile(string filePath) {
+				bool is_deleted = DeleteFile(filePath);
+				return Marshal.GetLastWin32Error();
+			}
 "@
 	}
 	process {
 		try {
-            $FilePath = (Resolve-Path -Path $FilePath -ErrorAction Stop).Path
-            if (Test-Path -Path $FilePath -PathType Leaf) {
-                Write-Verbose "Unblocking $FilePath"
-                $Win32_SUCCESS = 0
-                $Win32_FILE_NOT_FOUND = 2
-                $zone_id_path = $FilePath + ':Zone.Identifier'
-                $result_code = [Win32.pinvoke]::Win32DeleteFile($zone_id_path)
-                if (-not ($Win32_SUCCESS,$Win32_FILE_NOT_FOUND) -contains $result_code) {
-                    Write-Error "Failed to unblock '$FilePath' the Win32 return code is '$result_code'."
-                }
-            } else { Write-Verbose "Ignoring non-file $FilePath" }
-        } catch { Write-Error ("Failed to unblock file. The error was: '{0}'." -f $_) } 
-    }
+			$FilePath = (Resolve-Path -Path $FilePath -ErrorAction Stop).Path
+			if (Test-Path -Path $FilePath -PathType Leaf) {
+				Write-Verbose "Unblocking $FilePath"
+				$Win32_SUCCESS = 0
+				$Win32_FILE_NOT_FOUND = 2
+				$zone_id_path = $FilePath + ':Zone.Identifier'
+				$result_code = [Win32.pinvoke]::Win32DeleteFile($zone_id_path)
+				if (-not ($Win32_SUCCESS,$Win32_FILE_NOT_FOUND) -contains $result_code) {
+					Write-Error "Failed to unblock '$FilePath' the Win32 return code is '$result_code'."
+				}
+			} else { Write-Verbose "Ignoring non-file $FilePath" }
+		} catch { Write-Error ("Failed to unblock file. The error was: '{0}'." -f $_) } 
+	}
 }
--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,6 @@
 function which ($command) {
-    Get-Command -Name $command -ErrorAction SilentlyContinue | 
-        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+	Get-Command -Name $command -ErrorAction SilentlyContinue | 
+		Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
 function Unblock-File {
@@ -10,29 +10,29 @@ function Unblock-File {
 	)
 	begin {
 		Add-Type -Namespace Win32 -Name pinvoke -MemberDefinition @"
-            // http://msdn.microsoft.com/en-us/library/windows/desktop/aa363915(v=vs.85).aspx
-            [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+			// http://msdn.microsoft.com/en-us/library/windows/desktop/aa363915(v=vs.85).aspx
+			[DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
 			[return: MarshalAs(UnmanagedType.Bool)]
 			private static extern bool DeleteFile(string name);
-            public static int Win32DeleteFile(string filePath) {
-                bool is_deleted = DeleteFile(filePath);
-                return Marshal.GetLastWin32Error();
-            }
+			public static int Win32DeleteFile(string filePath) {
+				bool is_deleted = DeleteFile(filePath);
+				return Marshal.GetLastWin32Error();
+			}
 "@
 	}
 	process {
 		try {
-            $FilePath = (Resolve-Path -Path $FilePath -ErrorAction Stop).Path
-            if (Test-Path -Path $FilePath -PathType Leaf) {
-                Write-Verbose "Unblocking $FilePath"
-                $Win32_SUCCESS = 0
-                $Win32_FILE_NOT_FOUND = 2
-                $zone_id_path = $FilePath + ':Zone.Identifier'
-                $result_code = [Win32.pinvoke]::Win32DeleteFile($zone_id_path)
-                if (-not ($Win32_SUCCESS,$Win32_FILE_NOT_FOUND) -contains $result_code) {
-                    Write-Error "Failed to unblock '$FilePath' the Win32 return code is '$result_code'."
-                }
-            } else { Write-Verbose "Ignoring non-file $FilePath" }
-        } catch { Write-Error ("Failed to unblock file. The error was: '{0}'." -f $_) } 
-    }
+			$FilePath = (Resolve-Path -Path $FilePath -ErrorAction Stop).Path
+			if (Test-Path -Path $FilePath -PathType Leaf) {
+				Write-Verbose "Unblocking $FilePath"
+				$Win32_SUCCESS = 0
+				$Win32_FILE_NOT_FOUND = 2
+				$zone_id_path = $FilePath + ':Zone.Identifier'
+				$result_code = [Win32.pinvoke]::Win32DeleteFile($zone_id_path)
+				if (-not ($Win32_SUCCESS,$Win32_FILE_NOT_FOUND) -contains $result_code) {
+					Write-Error "Failed to unblock '$FilePath' the Win32 return code is '$result_code'."
+				}
+			} else { Write-Verbose "Ignoring non-file $FilePath" }
+		} catch { Write-Error ("Failed to unblock file. The error was: '{0}'." -f $_) } 
+	}
 }
