436ed8434cb52add78b9af985a829b4bd70a13e9
Andy Arismendi <andy_arismendi@hotmail.com>
2013-03-08 22:11:34 -0500

    updated profile with latest unblock-file

--- profile.ps1
+++ profile.ps1
@@ -13,10 +13,12 @@ function Unblock-File {
 			public static int GetLastWin32Error() {
 				return Marshal.GetLastWin32Error();
 			}
+
             // http://msdn.microsoft.com/en-us/library/windows/desktop/aa363915(v=vs.85).aspx
             [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
 			[return: MarshalAs(UnmanagedType.Bool)]
 			private static extern bool DeleteFile(string name);
+
             public static bool Win32DeleteFile(string filePath) {
 				return DeleteFile(filePath);
 			}
@@ -25,13 +27,14 @@ function Unblock-File {
 	process {
 		try {
 			if (Test-Path -Path $FilePath -PathType Leaf) {
-				Write-Verbose "Unblocking $FilePath"
-				$zone_id_path = $FilePath + ':Zone.Identifier'
+                $full_path = Get-Item $FilePath | Select-Object -ExpandProperty FullName
+				Write-Verbose "Unblocking $full_path"
+				$zone_id_path = $full_path + ':Zone.Identifier'
 				$Win32_ERROR_FILE_NOT_FOUND = 2
 				if (-not [PsIO.File]::Win32DeleteFile($zone_id_path)) {
                     $last_error = [PsIO.File]::GetLastWin32Error()
 					if ($last_error -ne $Win32_ERROR_FILE_NOT_FOUND) {
-						Write-Error "Failed to unblock '$FilePath' the Win32 return code is '$last_error'."
+						Write-Error "Failed to unblock '$full_path' the Win32 return code is '$last_error'."
 					}
                 }
 			} else { Write-Verbose "Ignoring non-file $FilePath" }
