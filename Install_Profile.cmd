028ce27aeed25fa8e86f7cacdbb049c8af8acae1
Andy Arismendi <andy_arismendi@hotmail.com>
2013-03-27 01:11:07 -0400

    fixes

--- Install_Profile.cmd
+++ Install_Profile.cmd
@@ -1,2 +1,3 @@
-powershell.exe -ExecutionPolicy ByPass -Command "Set-ExecutionPolicy RemoteSigned"
+set PWD=%~dp0
+powershell.exe -executionpolicy bypass -command "&{import-module '%PWD%\Modules\Security\Security.psm1' -Force; dir '%PWD%' -rec | Unblock-File -Verbose}"
 powershell.exe -ExecutionPolicy ByPass -File "%~dp0\Install_Profile.ps1"
