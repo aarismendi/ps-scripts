f9f1c988726bfc988e27d5992517dabd48913335
Andy Arismendi <andy_arismendi@hotmail.com>
2013-05-20 23:57:33 -0400

    added unzip-file

--- profile.ps1
+++ profile.ps1
@@ -5,6 +5,45 @@ function which ($command) {
 
 Import-Module -Name Security -Force
 
+function Unzip-File {
+    [cmdletbinding(DefaultParameterSetName="ByPath", SupportsShouldProcess=$True)]
+    param (
+        [parameter(Mandatory=$true, ParameterSetName="ByPath", Position=0)] [string] $Path,
+        [parameter(Mandatory=$true, ParameterSetName="ByInput", ValueFromPipeline=$true)] $InputObject,
+        [string] $Destination,
+        [switch] $Force
+    )
+    begin {$shell_app = new-object -com shell.application}
+    process {
+        switch ($PSCmdlet.ParameterSetName) {
+            'ByPath'  {$input_paths = Resolve-Path -Path $Path | ? {[IO.File]::Exists($_.Path)} | Select -Exp Path}
+            'ByInput' {if ($InputObject -is [System.IO.FileInfo]) {$input_paths = $InputObject.FullName}}
+        }
+        $input_paths | % {   
+            $zip_file = $shell_app.namespace($_)
+            if (-not $Destination) {
+                $dest_home = (Get-Item -Path $_).Directory.FullName
+            } else {
+                $dest_home = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Destination)
+            }
+            $dest_name = [IO.Path]::GetFileNameWithoutExtension($_)
+            $zip_dest = [IO.Path]::Combine($dest_home, $dest_name)
+
+            if (-not (Test-Path -Path $zip_dest -PathType Container)) {
+                New-Item -Path $zip_dest -ItemType Directory -Force | Out-Null
+            }
+            $dest = $shell_app.namespace($zip_dest)
+            $NODIALOG = 0x4
+            $YESTOALL = 0x10
+            if ($Force) {
+                $dest.Copyhere($zip_file.items(), ($YESTOALL -bor $NODIALOG))
+            } else {
+                $dest.Copyhere($zip_file.items(), $NODIALOG)
+            }
+        }
+    }
+}
+
 function Out-Clipboard {
 	[cmdletbinding()]
 	param (
